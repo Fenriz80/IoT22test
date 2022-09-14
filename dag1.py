e3116907d71a4a41d9c910b7ccb7cc7cb5322627
Kristian Lönnsjö <k_lonnsjo@hotmail.com>
2022-09-14 14:27:05 +0200

    Test

---
+++ dag1.py
@@ -0,0 +1,34 @@
+"""
+# Detta är en kommentar
+print("Hello World!") # Skriver ut text
+print(42) # Skriver ut siffra
+print(1+9) # Skriver ut en addition
+
+
+# först skriver vi ut hej
+print("hej")
+# sen skriver vi ut summan av två tal
+print(33+200000)
+
+
+
+#variabler
+number = 42
+print("Variabeln 'number' innehåller", number)
+
+print(f"Variabeln 'number' innehåller {number}")
+
+
+
+number = 1
+#number = number + 1
+number += 1
+print(number)
+"""
+
+number = int(input("Enter a number: "))
+print(f"The number was : {number}")
+
+
+
+
